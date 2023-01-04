f6f8a89aa9a3a14b609e88561f9845fee6875923
novvval <nekittt1997@gmail.com>
2023-01-04 23:35:35 +0500

    settings changes

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from todolist.core.models import User
+from .models import User
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
 
+
+class PersonAdmin(admin.ModelAdmin):
+    list_filter = ['is_staff', 'is_active', "is_superuser"]
+    exclude = ('Password',)
+    readonly_fields = ["Last login", "Date joined"]
--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import environ
 from pathlib import Path
+import dj_database_url
 
 env = environ.Env(
     # set casting, default value
@@ -21,17 +22,20 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = env('DEBUG')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', 'sample_secret')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
@@ -46,7 +50,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "core",
-    "todolist"
 ]
 
 MIDDLEWARE = [
@@ -93,6 +96,7 @@ DATABASES = {
         'PORT': env("DB_PORT")
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
